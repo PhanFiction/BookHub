7f3d82f60291914d72857d60b34c47128cf3bee1
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-03 02:45:00 -0700

    Added types

---
+++ internal/types/types.go
@@ -0,0 +1,16 @@
+package types
+
+import "net/http"
+
+type BookDetails struct {
+	Title       string
+	Author      string
+	Pages       int
+	Publisher   string
+	ISBN        string
+	Description string
+	PublishedAt string
+}
+
+// Type function that takes an http.HandlerFunc and returns another http.HandlerFunc.
+type Middleware func(http.HandlerFunc) http.HandlerFunc
