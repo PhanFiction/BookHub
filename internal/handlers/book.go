33303b9f6fdf7d5592121d6b302683065a5e7176
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-05 22:10:30 -0700

    Updated FetchBookHandler, added UpdatedBookHandler, CreateBookHandler

--- internal/handlers/book.go
+++ internal/handlers/book.go
@@ -2,37 +2,37 @@ package handlers
 
 import (
 	"bookhub/internal/session"
+	"bookhub/internal/types"
 	"encoding/json"
 	"net/http"
 )
 
 // Return single book from the database
-func FetchBook(w http.ResponseWriter, r *http.Request) {
+func FetchBookHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := session.Store.Get(r, "session")
 	auth, ok := session.Values["authenticated"].(bool)
 
-	data := types.PageData{
-		TabTitle:      "About Page",
-		PageTitle:     "About",
+	data := types.Data{
+		BookData: types.BookDetails{
+			Title:       "The Great Gatsby",
+			Author:      "F. Scott Fitzgerald",
+			Pages:       180,
+			Publisher:   "Scribner",
+			ISBN:        "9780743273565",
+			Description: "A novel about the American dream.",
+			PublishedAt: "1925-04-10",
+		},
 		Authenticated: ok && auth,
 	}
 	json.NewEncoder(w).Encode(data)
 }
 
-func CreateBook(w http.ResponseWriter, r *http.Request) {
+func CreateBookHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := session.Store.Get(r, "session")
 	auth, ok := session.Values["authenticated"].(bool)
 
-	data := types.PageData{
-		TabTitle:      "About Page",
-		PageTitle:     "About",
+	data := types.Data{
 		Authenticated: ok && auth,
 	}
 	json.NewEncoder(w).Encode(data)
 }
-
-// Return Book based on Genre from the database
-
-// Return Book by author
-
-// Query Book in database
