c73374976012cf5a0956d40e79e7f5723711b572
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-05 22:10:47 -0700

    Updated HomeHandler

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -2,17 +2,16 @@ package handlers
 
 import (
 	"bookhub/internal/session"
+	"bookhub/internal/types"
 	"encoding/json"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := session.Store.Get(r, "session")
 	auth, ok := session.Values["authenticated"].(bool)
 
-	data := types.PageData{
-		TabTitle:      "About Page",
-		PageTitle:     "About",
+	data := types.Data{
 		Authenticated: ok && auth,
 	}
 	json.NewEncoder(w).Encode(data)
