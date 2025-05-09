0cffda82f94bc2eb42cf645c936a23b1af666d56
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-08 23:40:39 -0700

    Updated routes  - Changed /books/{title}/id/{id} to /books/{id}

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,7 +11,7 @@ import (
 
 func SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/books", middleware.Chain(handlers.FetchBookHandler, middleware.Logging()))
-	r.HandleFunc("/books/{title}/id/{id}", middleware.Chain(session.AuthMiddleware(handlers.FetchBookHandler), middleware.Logging()))
+	r.HandleFunc("/books/{id}", middleware.Chain(session.AuthMiddleware(handlers.FetchBookHandler), middleware.Logging()))
 	r.HandleFunc("/books/create-book", middleware.Chain(session.AuthMiddleware(handlers.CreateBookHandler), middleware.Logging()))
 	r.HandleFunc("/", middleware.Chain(handlers.HomeHandler, middleware.Logging())).Methods("GET")
 
