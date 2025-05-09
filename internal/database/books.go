29f4283a4ad6311f98d348d93b09383933874a81
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-08 17:28:11 -0700

    Updated books  - Updated FetchBook to retrieve books from the database

--- internal/database/books.go
+++ internal/database/books.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"bookhub/internal/types"
 	"database/sql"
 	"fmt"
 	"log"
@@ -8,30 +9,10 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// Fetch book from db based on data provided
-func FetchBook(query string, db *sql.DB) {
-	rows, err := db.Query(query)
-
-	if err != nil {
-		log.Fatal("Error fetching book:", err)
-	}
-
-	var title, author, publisher, isbn, description, publishedAt string
-	var pages int
-
-	for rows.Next() {
-		err := rows.Scan(&title, &author, &pages, &publisher, &isbn, &description, &publishedAt)
-		if err != nil {
-			log.Fatal("Error scanning book:", err)
-		}
-		fmt.Printf("Title: %s\nAuthor: %s\nPages: %d\nPublisher: %s\nISBN: %s\nDescription: %s\nPublished At: %s\n", title, author, pages, publisher, isbn, description, publishedAt)
-	}
-}
-
 func CreateBookTable(db *sql.DB) {
 	query := `
 	CREATE TABLE IF NOT EXISTS books (
-		id SERIAL PRIMARY KEY,
+		id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 		title TEXT NOT NULL,
 		author TEXT NOT NULL,
 		pages INT NOT NULL,
@@ -66,6 +47,58 @@ func CreateSavedBooksTable(db *sql.DB) {
 	}
 
 	fmt.Println("Saved books table created or already exists.")
+}
+
+// Fetch book from db based on data provided
+func FetchBook(db *sql.DB, query string) []types.BookDetails {
+	rows, err := db.Query(query) // Return all rows of books from the books table
+
+	if err != nil {
+		log.Fatal("Error fetching book:", err)
+	}
+
+	var title, author, publisher, isbn, description, publishedAt string
+	var id int
+	var pages int
+	book := []types.BookDetails{}
+
+	for rows.Next() {
+		err := rows.Scan(&id, &title, &author, &pages, &publisher, &isbn, &description, &publishedAt)
+		if err != nil {
+			log.Fatal("Error scanning book:", err)
+		}
+
+		book = append(book, types.BookDetails{
+			ID:          id,
+			Title:       title,
+			Author:      author,
+			Pages:       pages,
+			Publisher:   publisher,
+			ISBN:        isbn,
+			Description: description,
+			PublishedAt: publishedAt,
+		})
+
+		// fmt.Printf("Title: %s\nAuthor: %s\nPages: %d\nPublisher: %s\nISBN: %s\nDescription: %s\nPublished At: %s\n", title, author, pages, publisher, isbn, description, publishedAt)
+	}
+
+	return book
+}
+
+func CreateBook(db *sql.DB, BookDetails types.BookDetails) {
+	query := `
+		INSERT INTO books (title, author, pages, publisher, isbn, description, published_at)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		ON CONFLICT (isbn) DO NOTHING;
+	`
+	fmt.Println("Creating book:", BookDetails.Title)
+	_, err := db.Exec(query, BookDetails.Title, BookDetails.Author, BookDetails.Pages, BookDetails.Publisher, BookDetails.ISBN, BookDetails.Description, BookDetails.PublishedAt)
+
+	if err != nil {
+		log.Fatal("Error creating book:", err)
+	}
+
+	fmt.Println("Book created in database.")
 }
 
 func UpdateBook(db *sql.DB, bookID int, title, author string, pages int, publisher, isbn, description, publishedAt string) {
