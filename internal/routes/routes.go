d77fc3b6347127f439d3194add5499f8855e7ab0
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-10 23:03:26 -0700

    Updated routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,14 +10,18 @@ import (
 )
 
 func SetupRoutes(r *mux.Router) {
-	r.HandleFunc("/books", middleware.Chain(handlers.FetchBookHandler, middleware.Logging()))
-	r.HandleFunc("/books/{id}", middleware.Chain(session.AuthMiddleware(handlers.FetchBookHandler), middleware.Logging()))
+	r.HandleFunc("/books/{id}", middleware.Chain(handlers.FetchSingleBookHandler, middleware.Logging())).Methods("GET")
+	r.HandleFunc("/books/{id}", middleware.Chain(handlers.UpdateBookHandler, middleware.Logging())).Methods("PUT")
+	r.HandleFunc("/books/{id}", middleware.Chain(handlers.DeleteBookHandler, middleware.Logging())).Methods("DELETE")
 	r.HandleFunc("/books/create-book", middleware.Chain(session.AuthMiddleware(handlers.CreateBookHandler), middleware.Logging()))
+	r.HandleFunc("/books/update-book/{id}", middleware.Chain(session.AuthMiddleware(handlers.UpdateBookHandler), middleware.Logging()))
+	r.HandleFunc("/books", middleware.Chain(handlers.FetchBooksHandler, middleware.Logging()))
 	r.HandleFunc("/", middleware.Chain(handlers.HomeHandler, middleware.Logging())).Methods("GET")
 
 	// Auth
 	r.HandleFunc("/login", middleware.Chain(session.LoginHandler, middleware.Logging()))
 	r.HandleFunc("/logout", middleware.Chain(session.LogoutHandler, middleware.Logging()))
+	r.HandleFunc("/update-user", middleware.Chain(session.AuthMiddleware(session.UpdateUserHandler), middleware.Logging()))
 
 	// Serve CSS, JS, and images from static dir
 	fs := http.FileServer(http.Dir("static/"))
